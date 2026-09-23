bc2f42539cf40ba7bfbab9663cb2532bb76fe89d
David Calderón <dcalnav@gmail.com>
2026-09-23 18:17:07 +0200

    python

---
+++ ejercicios_python/holamundo.py
@@ -0,0 +1,30 @@
+print("hola clase")
+
+# Esto es un comentario
+
+a = 1 # Variable int
+b = 2.5 # Variable float
+c = "Hola" # Variable string
+d = True # Variable boolean
+
+print(type (a))
+print("Hola como estás\nPues aquí andamos")
+
+# Mostrar a como int y cambiarlo a String
+
+print(a)
+print(type(a))
+
+a = "Pepe"
+
+print(a)
+print(type(a))
+
+
+# Puede crear 3 varibales al mismo tiempo y mostralas juntas
+
+v1, v2, v3 = 1, 2, 3
+print(v1, v2, v3)
+
+numero = input("Introduce un numero: ")
+print(numero)
---
+++ ejercicios_python/main.py
@@ -0,0 +1,5 @@
+def main():
+    print("hola que tal")
+
+if __name__ == "__main__":
+    main()
